20cd6d5107bc2555f3eac2cd7ba7931519cdba9d
Martina Peraic <mperaic@gmail.com>
2019-06-17 15:27:04 +0200

    updated counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
 class Counter extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
+  render() {
+    return <h1>Hello World</h1>;
+  }
 }
- 
+
 export default Counter;
